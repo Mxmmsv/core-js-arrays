c794a3a6406340e48455c9a9d98c22e842140cf0
mxmmsv <oikioikioikiabrams@gmail.com>
2025-01-07 00:45:42 +0300

    feat: add createChunks task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -336,8 +336,16 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr.reduce((chunks, item, index) => {
+    const chunkIndex = Math.floor(index / chunkSize);
+    if (chunks[chunkIndex]) {
+      chunks[chunkIndex].push(item);
+    } else {
+      chunks.push([item]);
+    }
+    return chunks;
+  }, []);
 }
 
 /**
